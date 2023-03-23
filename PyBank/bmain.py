a5f8fe97bad201f030941b038f0e0fe35e3f7646
stephkyl <kyle.stephens@ucdenver.edu>
2023-03-22 20:58:55 -0600

    fixed the csv file import

--- PyBank/bmain.py
+++ PyBank/bmain.py
@@ -1,1 +1,23 @@
+#importing csv file from resources
+import csv
+
+with open("Resources/budget_data.csv", newline='') as budcsv:
+    infocsv = csv.reader(budcsv, delimiter=',')
+    header = next(infocsv)
+
+    date=[]
+    gains=[]
+
+#number of months in the csvfile
+    for row in infocsv:
+        date.append(row[0])
+        monthnum = len(date)
+
+        gains.append(row[1])
+    print(gains)
+    #print(monthnum)
+
+    
+
+
 
