cc8d1a6f94054ba261af8e4a4932db9d32694870
stephkyl <kyle.stephens@ucdenver.edu>
2023-03-29 18:24:35 -0600

    finished product

--- PyPoll/pmain.py
+++ PyPoll/pmain.py
@@ -7,9 +7,8 @@ with open("resources/election_data.csv") as electdata:
     bid =[]
     county =[]
     candidate = []
-    votec = 0
-    #wholoop = 0
     options =[]
+    
 
     #assigning the columns
     for row in electreader:
@@ -19,32 +18,71 @@ with open("resources/election_data.csv") as electdata:
 
     #making a count of the voter ids
     balcount = int(len(bid))
-    #print(balcount)
 
 #finding the available candidates that received votes
-# for element in candidate:
-#     if element not in options:
-#         options.append(element)
+for element in candidate:
+    if element not in options:
+        options.append(element)
+
+# options_two = options
+#options list ['Charles Casper Stockham', 'Diana DeGette', 'Raymon Anthony Doane']
+
+#finding the number of votes each candidate got
+optint = len(options) #made options an integer for the loops
+vote_per_can = []
+
+for canvote in range(optint):
+     vote_per_can.append(candidate.count(options[canvote]))
+
+# vote_per_can_two = vote_per_can
+
+pertcan = []
+
+#finding the percentage each candidate got
+for canper in range(optint):
+     pertcan.append(vote_per_can[canper]/balcount)
+
+# pertcan_two = pertcan
+#finding a winner loop
+winvotes = 0
+
+#finding the winner of the election
+for winloop in range(optint):
+     if vote_per_can[winloop]>winvotes:
+        winner = options[winloop]
+        winvotes = vote_per_can[winloop]
+
+#making the win percentage readable
+pertcan_format = ['%.2f' % elem for elem in pertcan]
+
+#using list() to make a zip that is printable
+# percentage_for_candidates = list(pertcan_format)
+# available_candidates = list(options)
+# votes_for_each_candidate = list(vote_per_can)
+# results = zip(available_candidates,percentage_for_candidates,votes_for_each_candidate)
+
+# print(options_two)
+# print(vote_per_can_two)
+# print(pertcan_format)
+# print(winner)
+# print(winvotes)
 
 
+ElectionResults = f'\
+Election Results!!\n\
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\
+Total Turnout of: {balcount}\n\
+The Election Results!:\n\
+{options}\n\
+{vote_per_can}\n\
+{pertcan_format}\n\
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\
+Congratulations {winner}\n\
+With {winvotes} Votes!!!'
 
+print(ElectionResults)
 
-
-
-
-  
-
+Analysisp = open("Analysisp/pypoll.txt","w")
+Analysisp.writelines(ElectionResults)
+Analysisp.close()
     
-
-    
-    
-
-    # canname =[]       
-    # canname.append(candidate[1])
-
-    # for who in range(balcount):
-    #     if candidate[who+1] != candidate[who] and candidate[who+1] not in canname:
-    #         canname.append(candidate[who+1])
-
-    # n = len(canname)
-    # print(n)
